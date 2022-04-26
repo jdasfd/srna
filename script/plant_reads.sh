693326ffc83c4b3d802b2d382681a2406bcabb8c
jdasfd-JYQ <55595834+jdasfd@users.noreply.github.com>
2022-04-26 22:25:26 +0800

    update bacteria alignment

--- script/plant_reads.sh
+++ script/plant_reads.sh
@@ -1,10 +1,13 @@
+echo "name,count,group"
 cd /mnt/e/project/srna/output/fastq
 for file in `ls *_plantall.fq.gz | perl -p -e 's/_.+gz$//'`
 do
 plant=`faops filter -l 0 ${file}_plantall.fq.gz stdout | grep '^>' | wc -l`;
-1mis=`faops filter -l 0 ${file}_plant1mis.fq.gz stdout | grep '^>' | wc -l`;
+mis1=`faops filter -l 0 ${file}_plant1mis.fq.gz stdout | grep '^>' | wc -l`;
 un=`faops filter -l 0 ${file}_plantunali.fq.gz stdout | grep '^>' | wc -l`;
-let no=${1mis}+${un};
+let no=${mis1}+${un};
 echo "${file},${plant},plant";
 echo "${file},${no},unknown";
 done
+
+# variable 1mis was not available
--- script/plant_rna_ratio.sh
+++ script/plant_rna_ratio.sh
@@ -1,11 +1,14 @@
 echo "name,count,group";
 cd /mnt/e/project/srna/output/bam/plant
-for file in `ls SRR*_plantall.sort.bam | perl -p -e 's/_plant.+bam$//'`
+for file in `ls SRR*_plantall1mis.sort.bam | perl -p -e 's/_plant.+bam$//'`
 do
 array=(trna rrna mrna mirna snrna snorna lncrna ncrna)
 for rna in ${array[@]}
 do
-count=`samtools view -@ 10 -L ../../../annotation/plant/Atha/Atha_${rna}.bed -F 4 -c ${file}_plantall.sort.bam`;
+count=`samtools view -@ 10 -L ../../../annotation/plant/Atha/Atha_${rna}.bed \
+${file}_plantall1mis.sort.bam | perl -n -e 'chomp;if($_=~/^@/){print "$_\n";}else{
+@array = split/\t/, $_;if($array[5] =~ /^((?!X).)*$/ && $array[1] != 4){print "$_\n";}else{next;}}
+| wc -l'`
 echo "${file},${count},${rna}";
 done
 done
